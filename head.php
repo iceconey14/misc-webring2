ce032b276d7cfaea5615c407308e1c87c9ac95b0
iceconey14 <147879255+iceconey14@users.noreply.github.com>
2024-04-16 20:49:34 -0700

    Create head.php

---
+++ head.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<html>
+<head>
+<title>misc-webring ~ t w o ~ </title> 
+</head>  
+<body> 
+<table border="5"><h1>the misc-webring ~two~</h1> <a href="member-list">members</a> | <a href="">embed</a></table>
